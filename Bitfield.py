2bf24a56d46638b19ac13359fe8d233f6148fcb5
davedave47 <149357865+davedave47@users.noreply.github.com>
2024-11-23 00:03:15 +0700

    Created a class for #1

---
+++ Bitfield.py
@@ -0,0 +1,16 @@
+from bitarray import bitarray
+
+class Bitfield:
+    def __init__(self, length, filled = True):
+        pass
+    def get_progress(self):
+        pass
+    def set_piece(self, index):
+        pass
+    def has_piece(self, index):
+        pass
+    def to_bytes(self):
+        pass
+    @staticmethod
+    def from_bytes(data, num_pieces):
+        pass
